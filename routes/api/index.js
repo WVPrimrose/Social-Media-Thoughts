4a7de6ad0df0abcde96ac312c0995dca0195f651
Katie Buchholz <kzbindy@hotmail.com>
2024-08-02 17:03:12 -0400

    changed the user routes file to users.js and changed the path from user-routes to /users

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require ('express').Router();
-const userRoutes = require('./user-routes')
+const userRoutes = require('./users')
 const thoughtRoutes = require('./thoughts')
 
-router.use('/user-routes', userRoutes)
+router.use('/users', userRoutes)
 router.use('/thoughts', thoughtRoutes)
 
 module.exports = router;
